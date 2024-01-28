8cd79c051d90f3b48ed856a94a46d27787f5c017
Raymond Opella <ray.opella@gmail.com>
2024-01-27 19:09:25 -0600

    Add self. references.

--- ConfigTab.py
+++ ConfigTab.py
@@ -14,38 +14,39 @@ class ConfigTab(ttk.Frame):
         self.subclasslevel.set(1)
 
         # Create widgets
-        label_classname = ttk.Label(self, text="Class name (Required):")
-        entry_classname = ttk.Entry(self, width=50)
-        label_subclassnames = ttk.Label(self,
-                                        text="Subclass name(s) (Optional):")
-        entry_subclassnames = ttk.Entry(self, width=100)
-        label_subclass_level = ttk.Label(self,
-                                         text="Subclass level (Optional):")
-        entry_subclass_level = ttk.Entry(self, width=5,
-                                         textvariable=self.subclasslevel)
+        self.label_classname = ttk.Label(self, text="Class name (Required):")
+        self.entry_classname = ttk.Entry(self, width=50)
+        self.label_subclassnames = ttk.Label(self,
+                                             text="Subclass name(s) (Optional):")
+        self.entry_subclassnames = ttk.Entry(self, width=100)
+        self.label_subclass_level = ttk.Label(self,
+                                              text="Subclass level (Optional):")
+        self.entry_subclass_level = ttk.Entry(self, width=5,
+                                              textvariable=self.subclasslevel)
 
         # Place widgets
-        label_classname.grid(column=0,
-                             row=0,
-                             padx=10,
-                             pady=5,
-                             sticky=tk.W)
-        entry_classname.grid(column=0,
-                             row=1,
-                             padx=10,
-                             pady=0,
-                             sticky=tk.W)
-        label_subclassnames.grid(column=0,
-                                 row=2,
-                                 padx=10,
-                                 pady=5,
-                                 sticky=tk.W)
-        entry_subclassnames.grid(column=0,
-                                 row=3,
-                                 padx=10,
-                                 pady=0, sticky=tk.W)
-        label_subclass_level.grid(column=1, row=2, padx=10, pady=5, sticky=tk.W)
-        entry_subclass_level.grid(column=1,
-                                  row=3,
+        self.label_classname.grid(column=0,
+                                  row=0,
                                   padx=10,
-                                  pady=0)
+                                  pady=5,
+                                  sticky=tk.W)
+        self.entry_classname.grid(column=0,
+                                  row=1,
+                                  padx=10,
+                                  pady=0,
+                                  sticky=tk.W)
+        self.label_subclassnames.grid(column=0,
+                                      row=2,
+                                      padx=10,
+                                      pady=5,
+                                      sticky=tk.W)
+        self.entry_subclassnames.grid(column=0,
+                                      row=3,
+                                      padx=10,
+                                      pady=0, sticky=tk.W)
+        self.label_subclass_level.grid(column=1, row=2, padx=10, pady=5,
+                                       sticky=tk.W)
+        self.entry_subclass_level.grid(column=1,
+                                       row=3,
+                                       padx=10,
+                                       pady=0)
