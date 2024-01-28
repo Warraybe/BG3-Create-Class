33cf37306f5ce02ee891aaa2c7cd7a4c39db5cb5
Raymond Opella <ray.opella@gmail.com>
2024-01-28 14:10:39 -0600

    Restructure file ad variables.

--- ClassConfigTab.py
+++ ClassConfigTab.py
@@ -8,42 +8,43 @@ class ClassConfigTab(ttk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.label_classname = ttk.Label(self, text="Class name (Required):")
-        self.entry_classname = ttk.Entry(self, width=50)
-        self.label_subclassnames = ttk.Label(self,
-                                        text="Subclass name(s) (Optional):")
-        self.entry_subclassnames = ttk.Entry(self, width=100)
-        self.label_subclass_level = ttk.Label(self,
-                                         text="Subclass level (Optional):")
-        self.entry_subclass_level = ttk.Entry(self, width=5)
+        self.classname = tk.StringVar(name="classname")
 
         self.place_widgets()
 
     def place_widgets(self):
+        label_classname = ttk.Label(self, text="Class name:")
+        entry_classname = ttk.Entry(self, width=50, textvariable=self.classname)
+        label_subclassnames = ttk.Label(self,
+                                        text="Subclass name(s) (Optional, comma separated):")
+        entry_subclassnames = ttk.Entry(self, width=100)
+        label_subclass_level = ttk.Label(self,
+                                         text="Subclass level (Optional, Defaults to 1):")
+        entry_subclass_level = ttk.Entry(self, width=5)
 
         # Place widgets
-        self.label_classname.grid(column=0,
+        label_classname.grid(column=0,
                              row=0,
                              padx=10,
                              pady=5,
                              sticky=tk.W)
-        self.entry_classname.grid(column=0,
+        entry_classname.grid(column=0,
                              row=1,
                              padx=10,
                              pady=0,
                              sticky=tk.W)
-        self.label_subclassnames.grid(column=0,
+        label_subclassnames.grid(column=0,
                                  row=2,
                                  padx=10,
                                  pady=5,
-                                 sticky=tk.W)
-        self.entry_subclassnames.grid(column=0,
+                                 sticky=tk.W, columnspan=2)
+        entry_subclassnames.grid(column=0,
                                  row=3,
                                  padx=10,
-                                 pady=0, sticky=tk.W)
-        self.label_subclass_level.grid(column=1, row=2, padx=10, pady=5,
+                                 pady=0, sticky=tk.W, columnspan=2)
+        label_subclass_level.grid(column=1, row=0, padx=10, pady=5,
                                   sticky=tk.W)
-        self.entry_subclass_level.grid(column=1,
-                                  row=3,
+        entry_subclass_level.grid(column=1,
+                                  row=1,
                                   padx=10,
                                   pady=0)
